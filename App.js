39b41aa169b15eed21005ecffd58c4c6c6a5b913
Ramadevi97 <106821749+Ramadevi97@users.noreply.github.com>
2024-03-14 09:58:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./Login";
+import Homepage from "./Homepage";
+
+import "./styles.css";
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? (
+                <Navigate to="/home" />
+              ) : (
+                <Login handleLogin={handleLogin} />
+              )
+            }
+          />
+          <Route
+            path="/home"
+            element={isLoggedIn ? <Homepage /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
